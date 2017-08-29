73484b6d8e0b3e78e94149c9f3bc2ba76af85499
Ulrich <backes@3u.net>
2017-08-29 17:55:50 +0200

    ExceptionHandling added

---
+++ 14ExceptionHandling/Computer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14ExceptionHandling
+{
+    class Computer
+    {
+        public double Divide(int a, int b)
+        {
+            try
+            {
+                //throw new FileNotFoundException("File not found", "myfile.txt");
+                File.ReadAllLines("myfile.txt");
+                double c = a / b;
+                return c;
+            }
+            catch (Exception e)
+            {
+                //throw new Exception("Mist", e);
+                //Console.WriteLine(e);
+                //Console.WriteLine();
+                //Console.WriteLine();
+                throw new Exception("My message", e);   // OK: Stack-Trace inklusive der Angabe der Zeilennummer
+                //throw;                        // NOK: Stack-Trace, verliert aber die Zeilennummer
+                //throw e;                      // NOK: Kein Stack-Trace
+
+            }
+            
+
+        }
+    }
+}
---
+++ 14ExceptionHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer comp = new Computer();
+            double d=0d;
+            try
+            {
+                d = comp.Divide(4, 0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("");
+                Console.WriteLine(e.Message);
+                //Console.WriteLine();
+                //Console.WriteLine(e.Message);
+                //Console.WriteLine();
+                //Console.WriteLine(e.GetType().FullName);
+
+            }
+            
+
+            Console.WriteLine($"d={d}");
+            Console.ReadKey();
+        }
+    }
+}
